f8d72af9e6f5a9a4b35bcce3aa88c5bd246d23ce
babarot <b4b4r07@gmail.com>
2025-01-16 18:46:41 +0900

    Refactor

--- main.go
+++ main.go
@@ -102,7 +102,6 @@ func (c *CLI) build(path string, nums ...int) error {
 		fmt.Println(len(dirs))
 		return nil
 	}
-	var indexes []int
 	var dirs1 []string = strings.Split(path, "/")
 	var dirs2 []string
 	switch dirs1[0] {
@@ -122,55 +121,68 @@ func (c *CLI) build(path string, nums ...int) error {
 		}
 	}
 	if c.isRange {
-		start := nums[0]
-		end := nums[1]
+		var start, end = nums[0], nums[1]
 		switch {
 		case 0 < end && end < start:
 			return fmt.Errorf(
-				"right side (%d) in case of positive number should be smaller than left side (%d)",
+				"invalid range: right side (%d) should be smaller than left side (%d)",
 				start, end)
 		case end < 0:
-			end = len(dirs1) + 1 + end
-			if end < start {
-				return fmt.Errorf("index out of range (range should be -%d..%d)",
-					len(dirs1), len(dirs1))
+			positiveEnd := len(dirs1) + 1 + end
+			if positiveEnd < start {
+				return fmt.Errorf("index out of range %d: range should be -%d..%d", end, len(dirs1), len(dirs1))
 			}
+			end = positiveEnd
 		case end == 0:
 			end = len(dirs1)
-		case end > len(dirs1):
-			log.Printf("[ERROR] LEAKED! %d is bigger than length of given path (%d)", end, len(dirs1))
-			return nil
+		case len(dirs1) < end:
+			return fmt.Errorf("index out of range %d: larger than length of given path (%d)", end, len(dirs1))
 		}
+		var tmp []int
 		for i := start; i <= end; i++ {
-			indexes = append(indexes, i)
+			tmp = append(tmp, i)
 		}
-	} else {
-		for _, i := range nums {
+		nums = tmp
+	}
+	log.Printf("[DEBUG] %#v: nums is built", nums)
+	convertToPositives := func() ([]int, error) {
+		var indexes []int
+		for _, num := range nums {
 			switch {
-			case i < 0:
-				j := i + len(dirs1) + 1
-				log.Printf("[DEBUG] arg %d is negative number, so calculated from backward: %d", i, j)
-				if j < 0 {
-					log.Printf("[ERROR] %d: LEAKED! calculated result from backward is still negative, index out of range", j)
-					continue
+			case num < 0:
+				positiveNum := num + len(dirs1) + 1
+				log.Printf("[DEBUG] convert %d to %d (count from backward)", num, positiveNum)
+				if positiveNum < 0 {
+					log.Printf("[ERROR] %d: converted but still negative", positiveNum)
+					return []int{}, fmt.Errorf("index out of range %d: range should be -%d..%d", num, len(dirs1), len(dirs1))
 				}
-				indexes = append(indexes, j)
-			case i > len(dirs1):
-				log.Printf("[ERROR] LEAKED! %d is bigger than length of given path (%d)", i, len(dirs1))
-				continue
+				indexes = append(indexes, positiveNum)
+			case len(dirs1) < num:
+				return []int{}, fmt.Errorf("index out of range %d: larger than length of given path (%d)", num, len(dirs1))
 			default:
-				indexes = append(indexes, i)
+				indexes = append(indexes, num)
 			}
 		}
-	}
-	log.Printf("[DEBUG] indexes: %#v", indexes)
-	for _, idx := range indexes {
-		idx -= 1 // handle 1-origin
-		if dirs1[idx] == "" {
-			log.Printf("[DEBUG] removing a space from paths: %#v", dirs1)
-			continue // remove a space
+		pairs, err := zipmap(nums, indexes)
+		if err == nil {
+			for _, pair := range pairs {
+				log.Printf("[TRACE] converted %d -> %d\n", pair.a, pair.b)
+			}
 		}
-		dirs2 = append(dirs2, dirs1[idx])
+		return indexes, nil
+	}
+	nums, err := convertToPositives()
+	if err != nil {
+		return fmt.Errorf("cannot convert negative numbers: %w", err)
+	}
+	log.Printf("[DEBUG] %#v: nums is converted with positives", nums)
+	for _, num := range nums {
+		num -= 1 // convert 1-origin to 0-origin
+		if dirs1[num] == "" {
+			log.Printf("[DEBUG] removing a space from paths: %#v", dirs1)
+			continue
+		}
+		dirs2 = append(dirs2, dirs1[num])
 	}
 	fmt.Fprintf(c.Stdout, "%s\n", strings.Join(dirs2, "/"))
 	return nil
@@ -196,4 +208,22 @@ func logOutput() io.Writer {
 	}
 
 	return filter
+}
+
+type intTuple struct {
+	a, b int
+}
+
+func zipmap(a, b []int) ([]intTuple, error) {
+	if len(a) != len(b) {
+		return nil, fmt.Errorf("zipmap: arguments must be of same length")
+	}
+
+	r := make([]intTuple, len(a), len(a))
+
+	for i, e := range a {
+		r[i] = intTuple{e, b[i]}
+	}
+
+	return r, nil
 }
